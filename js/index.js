3db0d273d7a8433a5ac980c1aff779c98a90782f
emmaglorypraise <emmaglorypraise@gmail.com>
2022-05-28 00:09:19 +0100

    code update

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ const totalMoney = document.getElementById('totalGroupMoney');
 let amount = document.getElementById('amount');
 let small = document.getElementsByTagName('small');
 const msg = document.getElementById("small");
+let list = document.getElementById('list');
+let ridersList = [];
 let percent;
+let calAmount;
 
 totalMoney.innerHTML = ' N0.00';
  
@@ -19,11 +22,9 @@ form.addEventListener("submit", function (event) {
     }
     console.log(fullname);
 
-    // get tier
+    // get selected tier
     let tier = document.getElementById('SelectTier');
     let tierSelected = tier.options[tier.selectedIndex].value; //gets value
-    // var tierSelectedvalue = tier.options[tier.selectedIndex].text; //gets text
-    console.log(tierSelected);
 
     // checks percentage
     if (tierSelected === 'tier 1') {
@@ -36,20 +37,79 @@ form.addEventListener("submit", function (event) {
         alert('Select a tier!')
     }
 
-    // get current amount inputed
+    // get amount inputed and checks if it matches the tier  
     newAmount = amount.value;
+    
     if (newAmount == '' || newAmount.length == 0){
         alert('Please input your amount to save')
+    } else if (tierSelected === 'tier 1') {
+        if (newAmount <= 1000 || newAmount > 10000) {
+        alert("Amount must be greater than 1000 and not more than 10000");
+        msg.innerHTML = "Please try registering again";
+        location.reload()
+       } else {
+            alert('Good to go...');
+            calAmount = percent * newAmount;
+            calAmount = calAmount.toFixed(2);
+
+            // updates total money value
+            totalMoney.innerHTML = newAmount;
+
+            // shows percentage received
+            msg.innerHTML = "You will get N" + calAmount + ' weekly';
+
+            // pushes new rider into array of riders
+            ridersList.push(fullname) 
+            list.innerHTML = ridersList;
+       }
+    } else if (tierSelected === 'tier 2') {
+        if (newAmount <= 20000 || newAmount > 30000) {
+        alert("Amount must be greater than 20000 and not more than 30000");
+        msg.innerHTML = "Please try registering again";
+        location.reload()
+       } else {
+            alert('Good to go...');
+            calAmount = percent * newAmount;
+            calAmount = calAmount.toFixed(2);
+
+            // updates total money value
+            totalMoney.innerHTML = newAmount;
+
+            // shows percentage received
+            msg.innerHTML = "You will get N" + calAmount + ' weekly';
+
+            // pushes new rider into array of riders
+            ridersList.push(fullname) 
+            list.innerHTML = ridersList;
+   }
+    } else if (tierSelected === 'tier 3') {
+        if (newAmount >= 30000) {
+        alert("Amount must be greater than 30000")
+        msg.innerHTML = "Please try registering again";
+        location.reload()
+       } else {
+            alert('Good to go...');
+            calAmount = percent * newAmount;
+            calAmount = calAmount.toFixed(2);
+
+            // updates total money value
+            totalMoney.innerHTML = newAmount;
+
+            // shows percentage received
+            msg.innerHTML = "You will get N" + calAmount + ' weekly';
+
+            // pushes new rider into array of riders
+            ridersList.push(fullname) 
+            list.innerHTML = ridersList;
+   }
+    } else {
+        alert('Something is wrong, please try again');
+        location.reload()
+
     }
-    console.log(newAmount)
-    let calAmount = percent * newAmount;
-    calAmount = calAmount.toFixed(2);
-    console.log(calAmount, "amount");
+    
 
-    // updates input
-    totalMoney.innerHTML = newAmount;
+    
 
-    // prints percentage received
-    msg.innerHTML = "You will get N" + calAmount + ' weekly';
 
 });
